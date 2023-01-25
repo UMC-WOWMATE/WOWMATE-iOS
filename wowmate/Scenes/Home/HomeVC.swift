b8cf7616b677f173d5a11462d7554b783614be8f
lyoodong <dwryu3079@naver.com>
2023-01-25 15:43:24 +0900

    [feature] homeVC

--- wowmate/Scenes/Home/HomeVC.swift
+++ wowmate/Scenes/Home/HomeVC.swift
@@ -15,6 +15,7 @@ class HomeVC: UIViewController {
     // 생명주기와 관련된 메서드 (viewDidLoad, viewDidDisappear...)
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .red
     }
     
     // MARK: - Actions
@@ -25,4 +26,5 @@ class HomeVC: UIViewController {
     // 설정, 데이터처리 등 액션 외의 메서드를 정의
 
 }
+
 
