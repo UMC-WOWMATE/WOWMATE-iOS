04179aaa067bb91eb968f46e068392fea0489ccd
Doy Kim <kimdee.dev@gmail.com>
2023-02-01 00:02:11 +0900

    [Feature] #24 mockAPI에 담아둔 더미데이터를 받아오는 타겟타입 추가 정의

--- wowmate/Data/Model/PostAPI.swift
+++ wowmate/Data/Model/PostAPI.swift
@@ -10,15 +10,24 @@ import Moya
 
 enum PostAPI {
     case posts
+    case mockPosts
 }
 
 extension PostAPI: TargetType {
     var baseURL: URL {
-        return URL(string: ServiceAPI.baseURL)!
+        switch self {
+        case .posts:
+            return URL(string: ServiceAPI.baseURL)!
+        case .mockPosts:
+            return URL(string: "https://63ba608856043ab3c79a44ce.mockapi.io/api/v1")!
+        }
+        
     }
     var path: String {
         switch self {
         case .posts:
+            return "/posts"
+        case .mockPosts:
             return "/posts"
         }
     }
@@ -26,11 +35,15 @@ extension PostAPI: TargetType {
         switch self {
         case .posts:
             return .get
+        case .mockPosts:
+            return .get
         }
     }
     var task: Moya.Task {
         switch self {
         case .posts:
+            return .requestPlain
+        case .mockPosts:
             return .requestPlain
         }
     }
