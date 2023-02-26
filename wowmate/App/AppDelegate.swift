3e1cbfd9f478b9f8e3c4d30a0b6f3f726a781ad0
Doy Kim <kimdee.dev@gmail.com>
2023-02-26 18:16:01 +0900

    [Setting] #40 파이어베이스 초기화 코드를 AppDelegate에 추가

--- wowmate/App/AppDelegate.swift
+++ wowmate/App/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import IQKeyboardManagerSwift
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,7 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         IQKeyboardManager.shared.enable = true
         IQKeyboardManager.shared.enableAutoToolbar = false
         IQKeyboardManager.shared.shouldResignOnTouchOutside = true // 화면 아무 곳이나 터치 시 키보드 dismiss
-        
+        /** Firebase */
+        FirebaseApp.configure()
         return true
     }
 
