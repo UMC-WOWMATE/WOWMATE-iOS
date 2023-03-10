68a8fae2f9587b8dd4ce559ed5d75fbce1b61c93
jiwoong264 <103922140+jiwoong264@users.noreply.github.com>
2023-03-11 05:54:09 +0900

    [feat] 글 상세보기 뷰 거의 완성
    카테고리 버튼 여백, 댓글 전부분 제외 레이아웃까지 맞춤
    에셋도 고해상도로 교체

--- wowmate/Scenes/Home/HOME2/MatchCellDetailsVC_AloeStackView.swift
+++ wowmate/Scenes/Home/HOME2/MatchCellDetailsVC_AloeStackView.swift
@@ -48,7 +48,23 @@ class MatchCellDetailsVC_AloeStackView: AloeStackViewController {
             present(alertAction, animated: true)
         }
     
+    @objc func likeButtonPressed(_ sender:Any){
+            let alertAction = UIAlertController(title: "게시글을 추천하시겠습니까?", message: "", preferredStyle: UIAlertController.Style.alert)
     
+            alertAction.addAction(UIAlertAction(title: "취소", style: .default))
+            alertAction.addAction(UIAlertAction(title: "추천", style: .default, handler: { action in
+                //추천시 동작
+                let alert_done = UIAlertController(
+                    title: "게시글 추천은 업데이트 예정입니다.",
+                    message: nil,
+                    preferredStyle: .alert)
+                alert_done.addAction(UIAlertAction(title: "확인", style: .cancel, handler: nil))
+
+                self.present(alert_done, animated: true)
+            }))
+    
+            present(alertAction, animated: true)
+        }
     
     // MARK: - Helpers
     func getData() {
@@ -105,37 +121,25 @@ class MatchCellDetailsVC_AloeStackView: AloeStackViewController {
     
     func setUpNavigationbarView() {
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(image:UIImage(imageLiteralResourceName:"ic 1"), style: .plain, target:self , action: #selector(backButtonPressed))
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image:UIImage(imageLiteralResourceName:"ic"), style: .plain, target:self , action: #selector(moreButtonPressed))
     }
     
-    func setUp_Title_moreButton() {print(MatchCellDetailsTitle)
-        let Title_Row = UIStackView()
-        Title_Row.axis = .horizontal
-        Title_Row.translatesAutoresizingMaskIntoConstraints = false
-        
-        let TitleLabel = UILabel()
+    func setUp_Title_moreButton() { let TitleLabel = UILabel()
         TitleLabel.font =  .head
         TitleLabel.textColor = .black
         TitleLabel.text = MatchCellDetailsTitle
         TitleLabel.translatesAutoresizingMaskIntoConstraints = false
         
-        let MoreButton = UIButton()
-        MoreButton.translatesAutoresizingMaskIntoConstraints = false
-        MoreButton.setImage(UIImage(imageLiteralResourceName: "ic"), for: .normal)
-        MoreButton.addTarget(self, action: #selector(moreButtonPressed), for: .allEvents)
-        
-        Title_Row.addArrangedSubview(TitleLabel)
-        Title_Row.addArrangedSubview(MoreButton)
-        stackView.addRow(Title_Row)
-        stackView.hideSeparator(forRow: Title_Row)
+        stackView.addRow(TitleLabel)
+        stackView.setInset(forRow: TitleLabel, inset: UIEdgeInsets(top: 6, left: 24, bottom: 6, right: 16))
+        stackView.hideSeparator(forRow: TitleLabel)
     }
     
-    func setUp_Date_Like() {print(MatchCellDetailsDate)
-        print(MatchCellDetailslikeCount)
-        let Date_Like_Row = UIStackView()
+    func setUp_Date_Like() {
+        let Date_Like_Row = AloeStackView()
         Date_Like_Row.axis = .horizontal
-        Date_Like_Row.alignment = .leading
-        Date_Like_Row.distribution = .fillProportionally
-//        Date_Like_Row.spacing = 2
+        Date_Like_Row.rowInset = UIEdgeInsets(top: 6, left: 2, bottom: 6, right: 2)
+        Date_Like_Row.hidesSeparatorsByDefault = true
         Date_Like_Row.translatesAutoresizingMaskIntoConstraints = false
 
         let DateLabel = UILabel()
@@ -144,69 +148,34 @@ class MatchCellDetailsVC_AloeStackView: AloeStackViewController {
         DateLabel.text = MatchCellDetailsDate
         DateLabel.textAlignment = .left
         DateLabel.translatesAutoresizingMaskIntoConstraints = false
-        DateLabel.widthAnchor.constraint(equalToConstant: 86).isActive = true
+        
+        let dotView = UIImageView(image: UIImage(named: "ic 3"))
+        dotView.contentMode = .scaleAspectFit
+        dotView.translatesAutoresizingMaskIntoConstraints = false
 
         let LikeButton = UIButton()
         LikeButton.setImage(UIImage(imageLiteralResourceName: "ic 2"), for: .normal)
-//        LikeButton.widthAnchor.constraint(equalToConstant: 10.5).isActive = true
-        LikeButton.setTitle(MatchCellDetailslikeCount, for: .normal)
-        LikeButton.titleLabel?.font = .body_16R
-        LikeButton.setTitleColor(.black, for: .normal)
+        LikeButton.translatesAutoresizingMaskIntoConstraints = false
+        LikeButton.addTarget(self, action: #selector(likeButtonPressed), for: .allEvents)
 
-//        let LikeLabel = UILabel()
-//        LikeLabel.text = MatchCellDetailslikeCount
-//        LikeLabel.font = .body_16R
+        let LikeLabel = UILabel()
+        LikeLabel.text = MatchCellDetailslikeCount
+        LikeLabel.font = .body_16R
+        LikeLabel.textColor = .black
+        LikeLabel.translatesAutoresizingMaskIntoConstraints = false
 
 
-        Date_Like_Row.addArrangedSubview(DateLabel)
-        Date_Like_Row.addArrangedSubview(LikeButton)
-//        Date_Like_Row.addArrangedSubview(LikeLabel)
+        Date_Like_Row.addRow(DateLabel)
+        Date_Like_Row.addRow(dotView)
+        Date_Like_Row.addRow(LikeButton)
+        Date_Like_Row.addRow(LikeLabel)
         stackView.addRow(Date_Like_Row)
+        stackView.setInset(forRow: Date_Like_Row, inset: UIEdgeInsets(top: 6, left: 20, bottom: 6, right: 8))
         stackView.hideSeparator(forRow: Date_Like_Row)
     }
     
-//    func setUp_Date_Like() {
-//        let Date_Like_Row = UIView()
-//
-//        let DateLabel = UILabel()
-//        DateLabel.font =  .body_16R
-//        DateLabel.textColor = .black
-//        DateLabel.text = MatchCellDetailsDate
-//        DateLabel.widthAnchor.constraint(equalToConstant: 86).isActive = true
-//        DateLabel.leadingAnchor.constraint(equalTo: Date_Like_Row.leadingAnchor, constant: 22).isActive = true
-//        DateLabel.topAnchor.constraint(equalTo: Date_Like_Row.topAnchor, constant: 20).isActive = true
-//        DateLabel.backgroundColor = .green
-//
-//        Date_Like_Row.addSubview(DateLabel)
-////        let LikeButton = UIButton()
-////        Date_Like_Row.addSubview(LikeButton)
-////        LikeButton.setImage(UIImage(imageLiteralResourceName: "ic 2"), for: .normal)
-////        LikeButton.leadingAnchor.constraint(equalTo: DateLabel.trailingAnchor, constant: 30).isActive = true
-////        LikeButton.setTitle(MatchCellDetailslikeCount, for: .normal)
-////        LikeButton.titleLabel?.font = .body_16R
-////        LikeButton.setTitleColor(.black, for: .normal)
-////        LikeButton.backgroundColor = .green
-////        let LikeLabel = UILabel()
-////        LikeLabel.text = MatchCellDetailslikeCount
-////        LikeLabel.font = .body_16R
-////
-////        LikeStackView.addArrangedSubview(LikeButton)
-////        LikeStackView.addArrangedSubview(LikeLabel)
-//
-//
-////        Date_Like_Row.addArrangedSubview(dot)
-////        Date_Like_Row.addArrangedSubview(LikeStackView)
-//
-////        Date_Like_Row.addArrangedSubview(LikeLabel)
-//
-//        Date_Like_Row.heightAnchor.constraint(equalToConstant: 100).isActive = true
-////        Date_Like_Row.backgroundColor = .orange
-//        stackView.addRow(Date_Like_Row)
-//        stackView.hideSeparator(forRow: Date_Like_Row)
-//    }
     
-    func setUp_Category_Member() {print(MatchCellDetailsCategoty)
-        print(MatchCellDetailsMember)
+    func setUp_Category_Member() {
         let Category_Member_Row = UIStackView()
         Category_Member_Row.axis = .horizontal
         Category_Member_Row.alignment = .center
@@ -220,10 +189,7 @@ class MatchCellDetailsVC_AloeStackView: AloeStackViewController {
         CategoryButton.layer.cornerRadius = 14
         CategoryButton.setTitle(MatchCellDetailsCategoty, for: .normal)
         CategoryButton.setTitleColor(.white, for: .normal)
-//        CategoryButton.configuration
-//        CategoryButton.widthAnchor.constraint(equalToConstant: 96).isActive = true
-//        CategoryButton.heightAnchor.constraint(equalToConstant: 32).isActive = true
-//        CategoryButton.translatesAutoresizingMaskIntoConstraints = false
+        CategoryButton.translatesAutoresizingMaskIntoConstraints = false
 
         let MemberLabel = UILabel()
         if MatchCellDetailsMember == "무관" {
@@ -239,47 +205,16 @@ class MatchCellDetailsVC_AloeStackView: AloeStackViewController {
         Category_Member_Row.addArrangedSubview(MemberLabel)
 
         stackView.addRow(Category_Member_Row)
-        stackView.hideSeparator(forRow: Category_Member_Row)
+        stackView.setInset(forRow: Category_Member_Row, inset: UIEdgeInsets(top: 6, left: 16, bottom: 15.5, right: 8))
     }
     
-//    func setUp_Category_Member() {
-//        let Category_Member_Row = UIView()
-//        Category_Member_Row.translatesAutoresizingMaskIntoConstraints = false
-//
-//        let CategoryButton = UIButton()
-//        CategoryButton.backgroundColor = UIColor(r: 101, g: 81, b: 224)
-//        CategoryButton.titleLabel?.font = .body_14B
-//        CategoryButton.layer.cornerRadius = 14
-//        CategoryButton.setTitle(MatchCellDetailsCategoty, for: .normal)
-//        CategoryButton.setTitleColor(.white, for: .normal)
-//        CategoryButton.translatesAutoresizingMaskIntoConstraints = false
-//
-//        let MemberLabel = UILabel()
-//        if MatchCellDetailsMember == "무관" {
-//            MemberLabel.text = "모집인원 무관"
-//        }
-//        else {
-//            MemberLabel.text = "모집인원 " + MatchCellDetailsMember + "명"
-//        }
-//        MemberLabel.font = .body_16SB
-//        MemberLabel.textColor = .black
-//        MemberLabel.translatesAutoresizingMaskIntoConstraints = false
-//
-//        Category_Member_Row.addSubview(CategoryButton)
-//        Category_Member_Row.addSubview(MemberLabel)
-//
-//        NSLayoutConstraint.activate([
-//            MemberLabel.leadingAnchor.constraint(equalTo: Category_Member_Row.trailingAnchor, constant: 50)])
-//
-//        stackView.addRow(Category_Member_Row)
-//        stackView.hideSeparator(forRow: Category_Member_Row)
-//    }
-    
-    
     func setUp_Tags() {
+        if tagList[0] == "" { return }
+        
         let TagStackView = AloeStackView()
         TagStackView.axis = .horizontal
         TagStackView.hidesSeparatorsByDefault = true
+        TagStackView.rowInset = UIEdgeInsets(top: 0, left: 8, bottom:0, right: 8)
         TagStackView.translatesAutoresizingMaskIntoConstraints = false
         
         for tag in tagList {
@@ -297,6 +232,7 @@ class MatchCellDetailsVC_AloeStackView: AloeStackViewController {
         }
         
         stackView.addRow(TagStackView)
+        stackView.setInset(forRow: TagStackView, inset: UIEdgeInsets(top: 16, left: 8, bottom: 15.5, right: 8))
     }
     
     func setUp_Context() {
@@ -307,24 +243,26 @@ class MatchCellDetailsVC_AloeStackView: AloeStackViewController {
         ContextLabel.textColor = .black
         stackView.addRow(ContextLabel)
         stackView.hideSeparator(forRow: ContextLabel)
+        stackView.setInset(forRow: ContextLabel, inset: UIEdgeInsets(top: 16, left: 16, bottom: 4, right: 16))
         
         let ContextTextView = UITextView()
-//        ContextTextView.delegate = self
         ContextTextView.font = .body_14R
         ContextTextView.text = MatchCellDetailsContext
         ContextTextView.textColor = UIColor(r: 51, g: 51, b: 51)
         ContextTextView.textContainerInset = UIEdgeInsets(top: 12, left: 12, bottom: 12, right: 12)
         ContextTextView.layer.cornerRadius = 10
-        
         ContextTextView.backgroundColor = UIColor(r: 240, g: 240, b: 240)
-//        ContextTextView.heightAnchor.constraint(equalToConstant: 150).isActive = true
         ContextTextView.heightAnchor.constraint(greaterThanOrEqualToConstant: 150).isActive = true
         ContextTextView.translatesAutoresizingMaskIntoConstraints = false
+        ContextTextView.isEditable = false
         
         stackView.addRow(ContextTextView)
+        stackView.setInset(forRow: ContextTextView, inset: UIEdgeInsets(top: 4, left: 16, bottom: 15.5, right: 16))
     }
     
     func setUp_Image() {
+        if self.imageList.isEmpty { return }
+        
         let ImageLabel = UILabel()
         ImageLabel.font = .body_16B
         ImageLabel.textColor = UIColor(r: 0, g: 0, b: 0)
@@ -332,6 +270,7 @@ class MatchCellDetailsVC_AloeStackView: AloeStackViewController {
         ImageLabel.translatesAutoresizingMaskIntoConstraints = false
         stackView.addRow(ImageLabel)
         stackView.hideSeparator(forRow: ImageLabel)
+        stackView.setInset(forRow: ImageLabel, inset: UIEdgeInsets(top: 16, left: 16, bottom: 4, right: 16))
         
         let ImageStackView = AloeStackView()
         ImageStackView.axis = .horizontal
@@ -351,6 +290,7 @@ class MatchCellDetailsVC_AloeStackView: AloeStackViewController {
         }
         
         stackView.addRow(ImageStackView)
+        stackView.setInset(forRow: ImageStackView, inset: UIEdgeInsets(top: 4, left: 16, bottom: 15.5, right: 16))
         
     }
     
@@ -365,6 +305,7 @@ class MatchCellDetailsVC_AloeStackView: AloeStackViewController {
         
         let sample:String = "sample"
         
+        if self.comments.isEmpty { return }
         let firstrow = UIStackView()
                firstrow.axis = .horizontal
                firstrow.distribution = .fill
@@ -442,25 +383,6 @@ class MatchCellDetailsVC_AloeStackView: AloeStackViewController {
                
                stackView.addRow(thirdrow)
     }
-    
-    //    func row7(){
-    //
-    //        let tv = UITextView()
-    //        tv.textColor = UIColor.WM.gray700
-    //        tv.font = .body_14R
-    //        tv.backgroundColor = UIColor.WM.gray100
-    //
-    //        view.addSubview(tv)
-    //
-    //        tv.snp.makeConstraints { make in
-    //            make.leading.right.equalTo(stackView).offset(16)
-    //            make.trailing.equalTo(stackView).offset(-16)
-    //            make.height.equalTo(110)
-    //            make.top.equalTo(<#T##other: ConstraintRelatableTarget##ConstraintRelatableTarget#>)
-    //
-    //                }
-    //
-    //    }
     
     func test() {
         self.MatchCellDetailsTitle = "제목"
